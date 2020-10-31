47779016277535140a751f58c10554b8e042f8bc
Chin Hee Chen <hcchin@seekasia.com>
2020-10-31 15:10:36 +0800

    Initial commit

---
+++ resolvers.js
@@ -0,0 +1,12 @@
+const db = require("./db");
+const Query = {
+  greeting: () => {
+    return "hello from  TutorialsPoint !!!";
+  },
+  students: () => db.students.list(),
+  studentById: (root, args, context, info) => {
+    return db.students.get(args.id);
+  },
+};
+
+module.exports = { Query };
